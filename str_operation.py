aa1e6ebc938e5634eaa74eee70722ba92f75b016
andiainunnajib <andiainunajib59@gmail.com>
2021-06-06 13:26:30 +0700

    push baru str operation

--- str_operation.py
+++ str_operation.py
@@ -1,3 +1,11 @@
 a = "Hello, world!"
 print(a[0])
 print(a[1:4])
+print(len(a)) #untuk mengetahui len banyak kata dalam suatu string, jawabannya 13
+
+#nambah string
+a = "Hello"
+b = "World!"
+
+c = a + " " + b #tambahkan untuk kasih spasi
+print(c)
