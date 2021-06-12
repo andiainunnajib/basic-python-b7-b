43bfd1aaff94c910965364ba7ad68368b3080c94
andiainunnajib <andiainunajib59@gmail.com>
2021-06-12 14:40:19 +0700

    terbaru

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+array_nama = ['a','b','c']
+print(array_nama[0])
+print(array_nama[1])
+print(array_nama[2])
+
+print('=========')
+for i in array_nama: #pakai perulangan
+    print(i)
---
+++ range.py
@@ -0,0 +1,21 @@
+#start otomatis dari nol
+a = range(5) 
+print(a)
+print(type(a))
+
+for perulangan in a:
+    print(perulangan)
+
+print("===========")
+b = range(0,5)
+print(b)
+for i in b:
+    print(i)
+
+print("===========")
+
+
+#range(start, stop, step)
+c = range(1,10,2)
+for d in c:
+    print(d)
